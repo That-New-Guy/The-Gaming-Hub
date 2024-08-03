4d4870f2f3ec0b35ddd53f73f29b040559cce523
Ryan Pham <ryanking0823@gmail.com>
2024-08-03 18:03:16 +0700

    Fixed filing error

--- apply.js
+++ apply.js
@@ -3,13 +3,13 @@ const navBar = document.getElementById("nav-bar")
 navBar.innerHTML = `
     <button id="home-icon"></button>
     <div class="nav-bar-btn">
-        <a href="../index/index.html">
+        <a href="index.html">
             <button class="nav-btn">Home</button>
         </a>
-        <a href="../policy/policy.html">
+        <a href="policy.html">
             <button class="nav-btn">Policy</button>
         </a>
-        <a href="../apply/apply.html">
+        <a href="apply.html">
         <button class="nav-btn">Apply</button>
         </a>
     </div>`
