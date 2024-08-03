ac33aef88535a70dfbe8fa268e8468dd854e2fc6
Ryan Pham <ryanking0823@gmail.com>
2024-08-03 17:56:39 +0700

    acced function to buttons in navigation

---
+++ apply/apply.js
@@ -0,0 +1,15 @@
+const navBar = document.getElementById("nav-bar")
+
+navBar.innerHTML = `
+    <button id="home-icon"></button>
+    <div class="nav-bar-btn">
+        <a href="../index/index.html">
+            <button class="nav-btn">Home</button>
+        </a>
+        <a href="../policy/policy.html">
+            <button class="nav-btn">Policy</button>
+        </a>
+        <a href="../apply/apply.html">
+        <button class="nav-btn">Apply</button>
+        </a>
+    </div>`
