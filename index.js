9fd0975ccd957433d29745eb8553e2684dc41aa7
Ryan Pham <ryanking0823@gmail.com>
2024-08-03 15:17:45 +0700

    Added navigation bar

---
+++ index.js
@@ -0,0 +1,12 @@
+const navBar = document.getElementById("nav-bar")
+
+navBar.addEventListener("click", function() {
+
+})
+
+navBar.innerHTML = `
+    <button class="navigation" id="home-icon"></button>
+    <button class="navigation">Home</button>
+    <button class="navigation">Policy</button>
+    <button class="navigation">Apply</button>
+`
